e2cb755f37aa0f96bd90c45b3996d92c9bf21ad4
mixassio <mixail.bondarev@gmail.com>
2018-02-13 22:42:13 +0300

    part#4 release 1.1.3

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -2,4 +2,3 @@
 import evenNumber from '..';
 
 evenNumber();
-
--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-const answer = () => console.log(`Hi ${userName}!`);
-export default answer;
+const evenNumber = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi ${userName}!`);
+  console.log('Answer "yes" if number even otherwise answer "no".');
+  const answerList = ['yes', 'no'];
+  let trueAnswer;
+  let answer;
+  for (let i = 0; i < 3; i += 1) {
+    const qwestion = Math.floor(Math.random() * 10);
+    console.log('Question: ', qwestion);
+    answer = readlineSync.question('Your answer: ');
+    trueAnswer = qwestion % 2 === 0 ? 'yes' : 'no';
+    if (answerList.indexOf(answer) !== -1 && answer === trueAnswer) {
+      if (i === 2) {
+        console.log('Correct!');
+        console.log(`Congratulations, ${userName}!`);
+      } else {
+        console.log('Correct!');
+      }
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${trueAnswer}`);
+      console.log(`Let's try again, ${userName}!`);
+      break;
+    }
+  }
+};
+
+export default evenNumber;
